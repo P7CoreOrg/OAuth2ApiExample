f9e64fbe954bca5cdbce5cd9ece4f89eefa2da1a
Herb Stahl <ghstahl@gmail.com>
2019-10-07 13:57:21 -0700

    initial

---
+++ src/SecuredApiApp3/APIS/SimpleController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SecuredApiApp3.APIS
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleController : ControllerBase
+    {
+        private ILogger<SimpleController> _logger;
+
+        public SimpleController(ILogger<SimpleController> logger)
+        {
+            _logger = logger;
+        }
+        // GET: api/Simple
+        [HttpGet]
+        [Route("protected")]
+        [Authorize]
+        public IEnumerable<string> GetProtected()
+        {
+            return new string[] { "Protected", "Data" };
+        }
+        [HttpGet]
+        [Route("unprotected")]
+        public IEnumerable<string> GetUnprotected()
+        {
+            return new string[] { "Unprotected", "Data" };
+        }
+
+
+    }
+}
