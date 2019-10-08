b00018511c4b3ad4d7f318824c64304ded308afa
Herb Stahl <ghstahl@gmail.com>
2019-10-08 15:38:55 -0700

    added swagger and brought back a 2.2 app

--- src/SecuredApiApp22/Startup.cs
+++ src/SecuredApiApp22/Startup.cs
@@ -1,18 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
-namespace SecuredApiApp3
+namespace SecuredApiApp22
 {
     public class Startup
     {
@@ -26,73 +29,71 @@ namespace SecuredApiApp3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
+                  .AddJwtBearer(options =>
+                  {
 
-                    options.Authority = "https://localhost:6001/";
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
+                      options.Authority = "https://graphqlplay22.azurewebsites.net/";
+                      options.TokenValidationParameters = new TokenValidationParameters
+                      {
                         // Clock skew compensates for server time drift.
                         // We recommend 5 minutes or less:
                         ClockSkew = TimeSpan.FromMinutes(5),
-                        RequireSignedTokens = true,
+                          RequireSignedTokens = true,
                         // Ensure the token hasn't expired:
                         RequireExpirationTime = true,
-                        ValidateLifetime = true,
+                          ValidateLifetime = true,
                         // Ensure the token audience matches our audience value (default true):
                         ValidateAudience = false,
                         // Ensure the token was issued by a trusted authorization server (default true):
                         ValidateIssuer = true
 
-                    };
+                      };
 
-                });
-            services.AddControllers();
+                  });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "My API - V1",
+                        Version = "v1"
+                    }
+                 );
+
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "SecuredApiApp22.xml");
+                c.IncludeXmlComments(filePath);
             });
         }
+   
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-            // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseAuthentication();
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseMvc();
 
-            app.UseRouting();
-         
-            app.UseAuthorization();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
-            app.UseEndpoints(endpoints =>
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
             {
-                endpoints.MapControllers();
-                endpoints.MapRazorPages();
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SecuredApiApp22 V1");
             });
-
         }
     }
 }
